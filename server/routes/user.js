7127a19435ad57cb38b173a15281feb75ef39af8
Emmanuel <92612463+easaka@users.noreply.github.com>
2024-06-21 13:52:14 +0000

    Updated server

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,3 +5,4 @@ const router = express.Router()
 
 router.post('/register',register)
 
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const mongoString = "mongodb+srv://manuel:manuel123@cluster0.9fkysdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const userRoute = require('./routes/user.js')
 
 // console.log(mongoString);
 mongoose.connect(mongoString)
@@ -18,6 +19,8 @@ db.once('connected',(connected)=>{
 const app = express()
 
 app.use(express.json())
+
+app.use('/api/user', userRoute)
 
 
 
